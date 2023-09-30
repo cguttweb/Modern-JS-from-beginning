b89003882e117d8de75d57e9fc8bbde67db49906
Chloe <cguttweb@pm.me>
2023-09-30 12:01:42 +0100

    refactored single create item function into multiple for easier reuse

--- javascript-sandbox-start/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
+++ javascript-sandbox-start/06-document-object-model/09-refactor-to-multiple-functions/shopping-list/script.js
@@ -2,16 +2,28 @@ function createNewItem(item) {
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(item));
 
-  const button = document.createElement('button');
-  button.className = 'remove-item btn-link text-red';
-
-  const icon = document.createElement('i');
-  icon.className = 'fa-solid fa-xmark';
-
-  button.appendChild(icon);
+  const button = createNewButton('remove-item btn-link text-red')
   li.appendChild(button);
 
   document.querySelector('.items').appendChild(li);
 }
 
+function createNewButton(classes){
+  const button = document.createElement('button');
+  // 'remove-item btn-link text-red';
+  button.className = classes
+
+  const icon = createNewIcon('fa-solid fa-xmark')
+  button.appendChild(icon)
+
+  return button
+}
+
+function createNewIcon(classes){
+  const icon = document.createElement('i');
+  icon.className = classes
+  return icon
+}
+
 createNewItem('Cheese');
+createNewItem('tomatoes')
