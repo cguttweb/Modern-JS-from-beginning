a8388902b0597cc33a9f68c0505d2ad1882400ac
Chloe <cguttweb@pm.me>
2023-12-16 11:44:30 +0000

    learning basics of audio API

---
+++ javascript-sandbox-start/12-web-browser-apis/07-audio-api/script.js
@@ -0,0 +1,21 @@
+const audio = document.querySelector('#audio')
+const play = document.querySelector('#play')
+const pause = document.querySelector('#pause')
+const stop = document.querySelector('#stop')
+const currentTime = document.querySelector('#current-time')
+const volume = document.querySelector('#volume')
+
+play.addEventListener('click', () => audio.play())
+pause.addEventListener('click', () => audio.pause())
+stop.addEventListener('click', () => {
+  audio.pause()
+  audio.currentTime = 0
+})
+
+audio.addEventListener('timeupdate', () => {
+  currentTime.innerText = audio.currentTime
+})
+
+volume.addEventListener('change', () => {
+  (audio.volume = volume.value)
+})
